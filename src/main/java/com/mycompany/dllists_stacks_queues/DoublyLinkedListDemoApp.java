547be7d9899f19ae270e3004453e40c6ec2f9eeb
dandr <dandr@DESKTOP-D985USU>
2023-04-22 00:21:40 -0400

    lab 10

--- src/main/java/com/mycompany/dllists_stacks_queues/DoublyLinkedListDemoApp.java
+++ src/main/java/com/mycompany/dllists_stacks_queues/DoublyLinkedListDemoApp.java
@@ -35,5 +35,35 @@ public class DoublyLinkedListDemoApp {
       // Output final list
       System.out.print("List after removing nodes: ");
       numList.printList();
+       System.out.println();
+       
+       System.out.println("List in rev order");
+       numList.printRevList();
+       
+       System.out.println("Size");
+       numList.size();
+       // hw
+       //System.out.println("The min of the list is "+ numList.min());
+             // System.out.println("The max of the list is "+ numList.maax());
+             
+              DoublyLinkedList list = new DoublyLinkedList();
+   
+   list.append(new Node(1));
+   list.append(new Node(2));
+   list.append(new Node(3));
+   list.prepend(new Node(0));
+   list.insertAfter(list.head.next, new Node(5));
+   list.remove(list.head.next);
+   
+   list.printList(); // Output: 0 1 3
+   
+   int[] arr = list.toArray();
+   System.out.println(Arrays.toString(arr)); // Output: [0, 1, 3]
+   
+   System.out.println(list.indexOf(3)); // Output: 2
+   System.out.println(list.indexOf(5)); // Output: -1
+   
+   System.out.println(list.sumOfLastMElements(2)); // Output: 4
+
    }
 }
